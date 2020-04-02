3d5e072f6a8183db25c593d00c257704fa7c3c87
HrazderaAlexander <alexander.hrazdera@gmail.com>
2020-04-02 11:27:31 +0100

    Beschreibung

--- SmartFarm/pub.js
+++ SmartFarm/pub.js
@@ -1,28 +1,28 @@
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://52.157.91.193')
-var topic = 'sensors/hrazdera/ts1'
-var topicH = 'sensors/hrazdera/hs1'
+var client = mqtt.connect('mqtt://51.136.13.51')//mqtt server
+var topic = 'sensor/hrazdera/ts1'//topic Temperatur
+var topicH = 'sensor/hrazdera/hs1'//topic Temepartur
 
-        client.on('connect', ()=>{
+        client.on('connect', ()=>{ //verbindung
             setInterval(()=>{
                 var sensor = require("node-dht-sensor");
                 sensor.read(22, 4, function(err, temperature, humidity){
-                    if (!err){
-                        var message= {
+                    if (!err){ 
+                        var message= {//Json Object
                             type: "temperature",
                             value: temperature,
                             unit: "°C",
                         }
-                        var messageHumidity= {
+                        var messageHumidity= {//Json Object
                             type: "humidity",
                             value: humidity,
                             unit: "%"
                         }
-                        client.publish(topic, JSON.stringify(message))
-                        client.publish(topicH, JSON.stringify(messageHumidity))
-                        console.log('Message sent!', message)
-                        console.log('Message sent!', messageHumidity)
+                        client.publish(topic, JSON.stringify(message))//senden
+                        client.publish(topicH, JSON.stringify(messageHumidity))//senden
+                        console.log('Message sent!', message)//Ausgabe fuer mich
+                        console.log('Message sent!', messageHumidity)//Ausgabe fuer mich
                     }
                 })
-            }, 1000)
+            }, 1000)//Jede Sekunde wird gemessen
     })
